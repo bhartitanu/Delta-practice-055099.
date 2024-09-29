84633079324e87edd4f6660add90cbbaba987d47
Bharti Gupta <bharatbharti31@gmail.com>
2024-09-29 17:47:54 +0530

    Updated 	modified:   Dom Concept/assign-2/app.js 	modified:   Dom Concept/assign-2/index.html 	modified:   Dom Concept/event handling/app.js 	modified:   Dom Concept/event handling/index.html 	modified:   Dom Concept/event handling/style.css

--- Dom Concept/assign-2/app.js
+++ Dom Concept/assign-2/app.js
@@ -1,1 +1,29 @@
-console.log("practice Q-1");
+let btn=document.querySelector("button");
+let inp=document.querySelector("input");
+btn.addEventListener("mouseout",function()
+{
+    console.log("you are outside a button");
+});
+    
+inp.addEventListener("keypress",function(event){
+    console.log("you have press",event.key);
+});
+let para=document.querySelector('p');
+console.log(document.scrollingElement);
+document.addEventListener("scroll",function(){
+    document.scrollingElement=""
+});
+window.onload=alert("page is loaded");
+btn.addEventListener("click",function(){
+btn.style.backgroundColor="green";
+});
+let head=document.querySelector("h2");
+inp.addEventListener("keypress",function(event){
+    let key=event.code;
+    if((key >=65 && key<=90)||(key>=97 && key<=122)||key===32){
+        head.innerText=input.value+event.key;
+    }else{
+        event.preventDefault();
+    }
+});
+
--- Dom Concept/event handling/app.js
+++ Dom Concept/event handling/app.js
@@ -62,7 +62,22 @@ form.addEventListener("submit",function()
     pass.addEventListener("change",function(){
         console.log("the username is ",this.value);
     })
-    
+    let div=document.querySelector("#division");
+    ul=document.querySelector("ul");
+    lis=document.querySelectorAll("li");
+    div.addEventListener("click",function(event){
+        event.stopPropagation();
+        console.log("div is clicked");
+    }); 
+    ul.addEventListener("click",function(event){
+        event.stopPropagation();
+        console.log("ul is clicked");
+    });
+    for(li of lis){
+        li.addEventListener("click",function(){
+            console.log("li is clicked");
+        });
+    }
 
 
 
