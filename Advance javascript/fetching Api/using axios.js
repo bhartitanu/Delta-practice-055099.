13d0bfccb6e56b347890adb106857ecfc72eece7
Bharti Gupta <bharatbharti31@gmail.com>
2024-10-05 19:45:00 +0530

    modified:   Advance javascript/fetching Api/using axios.js

--- Advance javascript/fetching Api/using axios.js
+++ Advance javascript/fetching Api/using axios.js
@@ -9,3 +9,14 @@ async function getfacts(){
         console.log(error);
     }
 }
+let url2=" https://icanhazdadjoke.com/"
+async function getjoke(){
+    try{
+       const config={ header:{Accept:"application/json"} };
+       let res= await axios.get(url,config);
+       console.log(res.data[1].text);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
