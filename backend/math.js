556a2dd003e75b716e35bb245bce2daf2871498a
bhartitanu <bharatbharti31@gmail.com>
2024-10-16 15:07:50 +0530

    new file:   backend/math.js 	new file:   backend/script.js

---
+++ backend/math.js
@@ -0,0 +1,6 @@
+function add(x,y){
+    return x+y;
+}
+let pi=3.14;
+module.exports=add;
+module.exports=pi;
---
+++ backend/script.js
@@ -0,0 +1,15 @@
+console.log("HII");
+// console.log(process.cwd());
+// let n=8;
+// for(let i=0;i<n;i++){
+//     console.log("hello",i+1);
+// }
+//console.log(process.argv);
+let agrs= process.argv;
+for (let i=2;i<agrs.length;i++){
+  console.log("hello to",agrs[i]);
+}
+const read =require("./math");
+//console.log("reading",read.add(4,5));
+console.log(read);
+
