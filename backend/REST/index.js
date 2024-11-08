a16da274b39075869d73d00c81e239c07c74e3be
bhartitanu <bharatbharti31@gmail.com>
2024-11-08 23:45:22 +0530

    modified:   backend/REST/index.js 	new file:   backend/REST/node_modules/node/bin/node.exe 	new file:   backend/REST/node_modules/node/node_modules/node-win-x64/bin/node.exe 	modified:   backend/REST/package-lock.json 	modified:   backend/REST/package.json 	new file:   backend/REST/public/style.css 	modified:   backend/REST/views/index.ejs 	new file:   backend/REST/views/new.ejs

--- backend/REST/index.js
+++ backend/REST/index.js
@@ -7,10 +7,19 @@ app.use(express.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.set(express.static(path.join(__dirname,"public")));
-
-let post=[
+let posts=[
+    {
+        username:"teriin",
+        Skills:"exploring",
+        content :"Like to learn and explore"
+    },
+    {
+        username:"teesha",
+        Skills:"exploring,coding",
+        content :"Like to eat sweets and read while travelling"
+    },
     {
         username:"teriin",
         Skills:"exploring",
@@ -26,12 +35,19 @@ let post=[
         Skills:"Manufacturing",
         content :"Like to Marketing and gossiping"
     },
-]
+];
 
-app.get("/post",(req,res)=>{
-res.render("index.ejs");
+app.get("/posts",(req,res) => {
+res.render("index.ejs",{posts});
 });
-
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+app.post("/posts",(req,res)=>{
+    let {username,Skills,content}=req.body;
+    posts.push({username,Skills,content});
+    res.send("Post request Working");
+});
 app.listen(port,()=>{
     console.log(`App is listining on ${port}`);
 });
